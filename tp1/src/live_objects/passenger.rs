902cd9bf081b8fe402b42b9c5ef946b623517ee4
Matías Lafroce <matias@mini-parche>
2018-10-01 01:17:50 -0300

    Divide las funciones del barco en estados.

--- tp1/src/live_objects/passenger.rs
+++ tp1/src/live_objects/passenger.rs
@@ -1,18 +1,26 @@
+use concurrentes::ipc::named_pipe;
+
 use live_objects::lake::Lake;
 use live_objects::live_object::LiveObject;
 
 use std::cell::RefCell;
-use std::io::{Error, Write};
+use std::io::{Error, Write, Read, BufRead, BufReader};
 use std::process;
+
+use std::ops::Drop;
+
+use concurrentes::log::{GLOBAL_LOG, Log, LogSeverity};
 
 pub struct Passenger {
   destination: u32,
+  current_port: u32,
   id: u32
 }
 
 impl LiveObject for Passenger {
   fn tick(&mut self, lake: &RefCell<Lake>) -> Result<(), Error> {
     self.take_ship(lake);
+    self.wait_for_destination(lake);
     Ok(())
   }
 }
@@ -20,14 +28,43 @@ impl LiveObject for Passenger {
 
 impl Passenger {
   pub fn new() -> Passenger {
-    let destination = 0;
+    let current_port = 0;
+    let destination = 2;
     let id = process::id();
-    Passenger {destination, id}
+    let pipe_path = format!("passenger-{:?}.fifo", id);
+    named_pipe::NamedPipe::create(
+      pipe_path.as_str(), 0o0644).expect("Failed to create pipe");
+    Passenger {current_port, destination, id}
+  }
+
+  fn wait_for_destination(&mut self, lake: &RefCell<Lake>) {
+    let msg = format!("Esperando a llegar a destino {}",
+      self.destination);
+    log!(msg.as_str(), LogSeverity::INFO);
+    let pipe_path = format!("passenger-{:?}.fifo", self.id);
+    if let Ok(reader) = named_pipe::NamedPipeReader::open(pipe_path.as_str()) {
+      let mut buf_line = String::new();
+      let mut buf_reader = BufReader::new(reader);
+      let bytes_read = buf_reader.read_line(&mut buf_line);
+    }
   }
 
   fn take_ship(&mut self, lake: &RefCell<Lake>) {
+    let msg = format!("Tomando el barco en el puerto {}, destino {}",
+      self.current_port, self.destination);
+    log!(msg.as_str(), LogSeverity::INFO);
     let mut writer = lake.borrow_mut().
-      get_passenger_pipe_writer(0).expect("Failed to get pipe");
+      get_passenger_pipe_writer(self.current_port).expect("Failed to get pipe");
+    log!("Obtenido fifo", LogSeverity::DEBUG);
     writer.write_all(self.id.to_string().as_bytes()).expect("Failed to  write");
+    let writer_msg = format!("Datos enviados: {}", self.id.to_string());
+    log!(writer_msg.as_str(), LogSeverity::DEBUG);
+  }
+}
+
+impl Drop for Passenger {
+  fn drop(&mut self) {
+    let pipe_path = format!("passenger-{:?}.fifo", self.id);
+    named_pipe::NamedPipe::unlink(pipe_path.as_str()).unwrap();
   }
 }
--- tp1/src/live_objects/ship.rs
+++ tp1/src/live_objects/ship.rs
@@ -6,24 +6,43 @@ use concurrentes::log::{GLOBAL_LOG, Log, LogSeverity};
 use live_objects::lake::Lake;
 use live_objects::live_object::LiveObject;
 
-use std::io::{Error, Read};
+use std::io::{Error, Read, BufRead, BufReader};
 use std::time::Duration;
 use std::thread::sleep;
 use std::cell::RefCell;
 
 pub struct Ship {
   current_capacity: u32,
-  destination: u32
+  destination: u32,
+  status: Status
+}
+
+#[derive(Debug)]
+enum Status {
+  Travel,
+  PickPassengers,
+  Disembark
 }
 
 impl LiveObject for Ship {
   fn tick(&mut self, lake: &RefCell<Lake>) -> Result<(), Error> {
-    self.travel();
-    lake.borrow_mut().lock_port(self.destination)?;
-    self.pick_passengers(lake);
-    self.disembark();
-    lake.borrow_mut().unlock_port(self.destination)?;
-    self.destination = lake.borrow_mut().get_next_port(self.destination);
+    match self.status {
+      Status::Travel => {
+        self.travel(lake)?;
+        self.status = Status::PickPassengers;
+      },
+      Status::PickPassengers => {
+        if self.current_capacity > 0 {
+          self.pick_passenger(lake);
+        } else {
+          self.status = Status::Disembark;
+        }
+      },
+      Status::Disembark => {
+        self.disembark(lake)?;
+        self.status = Status::Travel;
+      }
+    }
     Ok(())
   }
 }
@@ -31,20 +50,22 @@ impl LiveObject for Ship {
 
 impl Ship {
   pub fn new(current_capacity: u32, destination: u32) -> Ship {
-    Ship {current_capacity: 2, destination: 0}
+    Ship {current_capacity, destination, status: Status::Travel}
   }
 
-  fn travel(&self) {
+  fn travel(&self, lake: &RefCell<Lake>) -> Result<(), Error> {
     let mut rng = rand::thread_rng();
-    let msecs = (rng.gen::<u32>() % 1000);
+    let msecs = rng.gen::<u32>() % 1000;
     let travel_time = Duration::from_millis(msecs as u64);
     let msg = format!("Viajando {} msecs al puerto {}",
       msecs, self.destination);
     log!(msg.as_str(), LogSeverity::INFO);
     sleep(travel_time);
+    lake.borrow_mut().lock_port(self.destination)?;
+    Ok(())
   }
 
-  fn disembark(&mut self) {
+  fn disembark(&mut self, lake: &RefCell<Lake>) -> Result<(), Error> {
     let mut rng = rand::thread_rng();
     let msecs = (rng.gen::<u32>() % 2000) + 500;
     let disembark_time = Duration::from_millis(msecs as u64);
@@ -53,32 +74,48 @@ impl Ship {
       msecs, self.current_capacity);
     log!(msg.as_str(), LogSeverity::INFO);
     sleep(disembark_time);
+    lake.borrow_mut().unlock_port(self.destination)?;
+    self.destination = lake.borrow_mut().get_next_port(self.destination);
+    Ok(())
   }
 
-  fn pick_passengers(&mut self, lake: &RefCell<Lake>) {
+  fn pick_passenger(&mut self, lake: &RefCell<Lake>) -> Option<u32> {
     log!("Obteniendo fifo", LogSeverity::DEBUG);
-    match lake.borrow_mut().get_passenger_pipe_reader(self.destination) {
-      Ok(mut reader) => {
-        while self.current_capacity > 0 {
-          let mut buf = String::new();
-          let bytes_read = reader.read_to_string(&mut buf);
-          match bytes_read {
-            Ok(_bytes) => {
-              let passenger_id = buf.parse::<u32>();
-              let msg = format!("Abordó el pasajero {:?}",
-                passenger_id);
-              log!(msg.as_str(), LogSeverity::INFO);
-              self.current_capacity -= 1;
-            },
-            Err(e) => log!(format!("{:?}", e).as_str(), LogSeverity::WARN),
+    let pipe_reader = lake.borrow_mut().get_passenger_pipe_reader(self.destination);
+    let passenger = match pipe_reader {
+      Ok(reader) => {
+        let mut buf_line = String::new();
+        let mut buf_reader = BufReader::new(reader);
+        let bytes_read = buf_reader.read_line(&mut buf_line);
+        let msg = format!("Levantando pasajero, leido {:?}",
+              buf_line);
+            log!(msg.as_str(), LogSeverity::DEBUG);
+        match bytes_read {
+          Ok(0) => None,
+          Ok(len) => {
+            let passenger_id = buf_line.parse::<u32>().unwrap();
+            let msg = format!("Abordó el pasajero {:?}",
+              passenger_id);
+            log!(msg.as_str(), LogSeverity::INFO);
+            self.current_capacity -= 1;
+            Some(passenger_id)
+          },
+          Err(e) => {
+            log!(format!("{:?}", e).as_str(), LogSeverity::WARN);
+            None
           }
         }
       }
       Err(e) => {
-        let msg = format!("Error al esperar pasajeros en el puerto {}: {:?}",
+        let msg = format!("Error al esperar pasajero en el puerto {}: {:?}",
           self.destination, e);
-        log!(msg.as_str(), LogSeverity::ERROR);;
+        log!(msg.as_str(), LogSeverity::ERROR);
+        None
       }
-    }
+    };
+    let msg = format!("Hay lugar para {:?} pasajeros",
+      self.current_capacity);
+    log!(msg.as_str(), LogSeverity::DEBUG);
+    None
   }
 }
